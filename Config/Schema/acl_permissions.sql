991b3442e7891fb46cabd82ecd21a5c51a7231dd
Jeremy Burns <jeremyburns@classoutfit.com>
2013-08-25 06:10:23 +0100

    Added a complete field that defaults to 0 and is set to 1 after processing.
    Added created and modified fields.

--- Config/Schema/acl_permissions.sql
+++ Config/Schema/acl_permissions.sql
@@ -5,5 +5,8 @@ CREATE TABLE `acl_permissions` (
   `action` varchar(100) DEFAULT NULL,
   `allow` varchar(20) DEFAULT NULL,
   `deny` varchar(20) DEFAULT NULL,
+  `complete` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
