1bb6a629ae91d9aed50ee316c8e527e5a937f0cc
Jeremy Burns <jeremyburns@classoutfit.com>
2014-01-06 14:27:52 +0000

    Added a beforeFind function that reads the userRoleModel variable to set the underlying table for the model.

--- Model/AclGroup.php
+++ Model/AclGroup.php
@@ -4,7 +4,11 @@ App::uses('AppModel', 'Model');
 class AclGroup extends AclPermissionAppModel {
     public $actsAs = array('Acl' => array('type' => 'requester'));
 
-    public $useTable = 'groups';
+    public $useTable = false;
+
+    public function beforeFind($query = array()) {
+		$this->useTable = Inflector::tableize(Configure::read('userRoleModel'));
+    }
 
     public function parentNode() {
         return null;
